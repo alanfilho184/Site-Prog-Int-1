4810f583e4d3cc197dcb159d710c90dd3e0ba7cf
Kamikaze_184 <alan.filho@hotmail.com>
2022-03-21 11:37:34 -0300

    v1.0.2 - Sombreado no hover

--- scripts/colorChange.js
+++ scripts/colorChange.js
@@ -1,4 +1,5 @@
 setInterval(() => {
     const actualColor = window.getComputedStyle(document.querySelector("main"), null).getPropertyValue('border-color');
     document.documentElement.style.setProperty('--text-hover', actualColor);
+    document.documentElement.style.setProperty('--shadow-color', actualColor);
 }, 500)
